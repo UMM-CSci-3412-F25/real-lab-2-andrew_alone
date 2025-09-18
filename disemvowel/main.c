ec4b1fbb74f0b2ddadf9c0cecf5d1f56ae5e86be
AndrewLam288 <lam00139@morris.umn.edu>
2025-09-18 15:08:47 -0500

    adjust getline to be leak free, pass local test for disemvowel-main-valgrind tests with no leaks

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "disemvowel.h"
 
 int main(int argc, char *argv[]) {
-  char *line;
-  size_t size;
-  
-  size = 100;
-  line = (char*) malloc (size + 1);
+  char *line = NULL;
+  size_t size = 0;
 
+  // read lines from standard input until EOF
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+    char *res = disemvowel(line);
+    if (res) {
+      printf("%s\n", res);
+      free(res);
+    }
   }
+  
+  free(line);
+  return 0;
 }
